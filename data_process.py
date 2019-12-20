d29797bfeddf03f0a6cddaa12ae5e3d22a83abb6
Yi君 <fangshijie_008@qq.com>
2019-12-20 17:30:18 +0800

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -12,19 +12,25 @@ pd.set_option('display.max_columns', None)
 
 
 churn = pd.read_csv('churn.csv',encoding='utf-8')#是否丢失
-custcall = pd.read_csv('custcall.csv')
-custinfo =  pd.read_csv('custinfo.csv')
-tariff = pd.read_csv('tariff.csv')
+custcall = pd.read_csv('custcall.csv')#电话记录
+custinfo =  pd.read_csv('custinfo.csv')#客户信息
+tariff = pd.read_csv('tariff.csv')#套餐信息
 
 #print(churn)
 #print(custcall)
 #print(custinfo)
 #print(tariff)
 #按照id合并电话
-custcall_merge = custcall[['Peak_calls',  'Peak_mins', 'OffPeak_calls' ,'OffPeak_mins',
-                     'Weekend_calls' ,'Weekend_mins','International_mins','Nat_call_cost', 'month'
-                     ]].groupby(custcall['Customer_ID']).sum()
+custcall_merge = custcall.groupby(custcall['Customer_ID'],as_index=False).sum()
 
-print(churn)
-print(custcall_merge)
-print(custinfo)
+#print(churn['Customer_ID'])
+#print(custcall_merge)
+#print(custcall_merge['Customer_ID'])
+
+#找出没有电话记录的标号
+subtraction = list(set(churn['Customer_ID']).difference(set(custcall_merge['Customer_ID'])))#在前不在后
+print(subtraction)
+#在churn里面剔除没有电话记录的标号
+print(churn[churn['Customer_ID']=='K244390'])
+#churn_delnone = churn.drop
+#异常值检测
